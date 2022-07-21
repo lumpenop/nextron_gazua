3d89ebfa465a7449f535baf34961190e968c90ce
lumpenop <swk5373@gmail.com>
2022-07-22 07:25:09 +0900

    Add: localstorage 기능 추가, home 컴포넌트에 alert 기능 추가

--- renderer/components/Modal.tsx
+++ renderer/components/Modal.tsx
@@ -4,16 +4,27 @@ import Sign from "./Sign";
 
 import { ipcRenderer } from "electron";
 
+import store from "store";
+
 interface Props {
   open: boolean;
   handleClose: () => void;
 }
 
 const Modal = ({ open, handleClose }: Props) => {
+  const storeInput = (userId, password) => {
+    const userInfo = {};
+    userInfo[userId] = password;
+    store.set("user", userInfo);
+  };
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    ipcRenderer.send("SIGN_UP", [data.get("userId"), data.get("password")]);
+    const userId = String(data.get("userId"));
+    const password = data.get("password");
+
+    ipcRenderer.send("SIGN_UP", [userId, password]);
+    storeInput(userId, password);
   };
   return (
     <div>
--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from "next/head";
-import { Theme, makeStyles, createStyles } from "@material-ui/core/styles";
-import { Button } from "@material-ui/core";
+import { useRouter } from "next/router";
 
-import Link from "../components/Link";
+import { Theme, makeStyles, createStyles } from "@material-ui/core/styles";
+import { Button, Dialog, Box } from "@material-ui/core";
+
 import Modal from "../components/Modal";
 
 import { ipcRenderer } from "electron";
 
 import Sign from "../components/Sign";
+
+import store from "store";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -21,16 +24,27 @@ const useStyles = makeStyles((theme: Theme) =>
 
 function Home() {
   const classes = useStyles({});
-  const [open, setOpen] = React.useState(false);
-  const handleClose = () => setOpen(false);
-  const handleClick = () => {
-    setOpen(true);
+  const router = useRouter();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [alertOpen, setAlertOpen] = useState(false);
+  const handleModalClose = () => setIsModalOpen(false);
+  const handleModalClick = () => {
+    setIsModalOpen(true);
   };
+  const alertClose = () => setAlertOpen(false);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    ipcRenderer.send("SIGN_IN", [data.get("userId"), data.get("password")]);
+    const userInfo = store.get("user");
+
+    if (userInfo[String(data.get("userId"))]) {
+      ipcRenderer.send("SIGN_IN", true);
+      router.push("/list");
+    } else {
+      setAlertOpen(true);
+      ipcRenderer.send("SIGN_IN", false);
+    }
   };
   return (
     <React.Fragment>
@@ -38,16 +52,26 @@ function Home() {
         <title>Home - Nextron (with-typescript-material-ui)</title>
       </Head>
       <div className={classes.root}>
+        <Dialog open={alertOpen} onClose={alertClose}>
+          <Box
+            sx={{
+              padding: "10px",
+            }}
+          >
+            아이디와 비밀번호를 확인해주세요.
+          </Box>
+        </Dialog>
         <div style={{ textAlign: "right", padding: "20px" }}>
-          <Button variant="contained" color="secondary" onClick={handleClick}>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={handleModalClick}
+          >
             Sign Up
           </Button>
         </div>
         <Sign handleSubmit={handleSubmit} signText="Sign In" />
-        <Modal open={open} handleClose={handleClose} />
-        <div style={{ marginTop: "10px" }}>
-          <Link href="/list">Go to the next page</Link>
-        </div>
+        <Modal open={isModalOpen} handleClose={handleModalClose} />
       </div>
     </React.Fragment>
   );
